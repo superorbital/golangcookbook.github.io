cd08be960f1eac61ae4adee39ba985a361e0530d
Tammer Saleh <tsaleh@gmail.com>
2015-06-17 14:48:56 +0100

    Reverse strings

---
+++ chapters/strings/reverse/test_reverse_by_character.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func reverse(s string) string {
+	chars := []rune(s)
+	for i, j := 0, len(chars)-1; i < j; i, j = i+1, j-1 {
+		chars[i], chars[j] = chars[j], chars[i]
+	}
+	return string(chars)
+}
+
+func main() {
+	fmt.Printf("%v\n", reverse("abcdefg"))
+}
---
+++ chapters/strings/reverse/test_reverse_by_word.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func reverse_words(s string) string {
+	words := strings.Fields(s)
+	for i, j := 0, len(words)-1; i < j; i, j = i+1, j-1 {
+		words[i], words[j] = words[j], words[i]
+	}
+	return strings.Join(words, " ")
+}
+
+func main() {
+	fmt.Println(reverse_words("one two three"))
+}
